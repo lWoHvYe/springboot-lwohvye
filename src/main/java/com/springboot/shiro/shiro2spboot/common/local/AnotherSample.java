e46cc13eba63ae4b6adbe78ae902c3e148f4487e
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2019-09-24 15:37:22 +0800

    调整开启线程及处理返回数据的方式，提高项目的灵活性，但当前用时过高，1百万次模拟，开5个线程，用时60000ms，相应的另一个方法，只用时52000ms，需进一步进行优化

--- src/main/java/com/springboot/shiro/shiro2spboot/common/local/AnotherSample.java
+++ src/main/java/com/springboot/shiro/shiro2spboot/common/local/AnotherSample.java
@@ -13,18 +13,19 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 /**
- * @PackageName: com.springboot.shiro.shiro2spboot.common.local
- * @ClassName: AnotherSample
- * @Description:抽卡模拟 将抽卡简化成随机取一个1000的样本中的数，取到指定的算抽中
+ * @author Hongyan Wang
+ * @packageName com.springboot.shiro.shiro2spboot.common.local
+ * @className AnotherSample
+ * @description 抽卡模拟 将抽卡简化成随机取一个1000的样本中的数，取到指定的算抽中
  * 在取到需要的时，会将与其同样的从期望中一并移除
  * 由于模拟采用了随机数的方式，所以池子可以任意配置，不影响结果
  * 由于使用了多线程，所以需关注其他线程的完成情况
  * 采用Feature的方式
  * 经过调整使用ThreadLocal修饰变量，简化线程内各函数的传值，但会一定程度上降低效率
  * 需尤其注意变量的作用范围问题
- * @Author: Hongyan Wang
- * @Date: 2019/9/22 8:54
+ * @date 2019/9/22 8:54
  */
+//TODO 该方式并没有线程间的共享数据，所以不会出现线程安全问题，但可能有一定的局限性
 //TODO 设置传入概率及池子的方法
 //TODO 优化统计中的if else
 //@SpringBootTest
@@ -44,21 +45,27 @@ public class AnotherSample {
     private ThreadLocal<Integer> s500 = ThreadLocal.withInitial(() -> 0);
     private ThreadLocal<Integer> other = ThreadLocal.withInitial(() -> 0);
 
+    //    Map的key和value
+    private String[] keys = new String[]{"s50", "s100", "s150", "s200", "s250", "s300", "s350", "s400", "s450", "s500", "other"};
+    private ThreadLocal[] values = new ThreadLocal[]{s50, s100, s150, s200, s250, s300, s350, s400, s450, s500, other};
+
 
     /**
-    * @Description: 方法主体，用于模拟调用，获取及输出模拟结果
-    * @Param: []
-    * @return: void
-    * @Author: Hongyan Wang
-    * @Date: 2019/9/23 9:59
-    */
+     * @return void
+     * @description 方法主体，用于模拟调用，获取及输出模拟结果
+     * @params []
+     * @author Hongyan Wang
+     * @date 2019/9/23 9:59
+     */
     @Test
     @SuppressWarnings("unchecked")
     public void startWork() throws ExecutionException, InterruptedException {
 //        存放总结果集
         Map<String, Integer> countMap = new HashMap<>();
-//        单个池子的模拟次数
-        Integer simuCount = 1000000;
+        //   开启模拟线程数
+        Integer threadCount = 100;
+//        模拟次数
+        Integer simCount = 1000000;
 //        记录开始时间
         long start = DateTimeUtil.getCurMilli();
         AnotherSample sample = new AnotherSample();
@@ -77,36 +84,60 @@ public class AnotherSample {
         lists.add(list2);
 //        lists.add(list3);
 //        设置模拟池子
-        SimuCallable simuCallable = new SimuCallable(lists, simuCount / 5);
+        SimCallable simCallable = new SimCallable(lists, simCount / threadCount);
+//        创建线程数组
+        CompletableFuture<Map<String, Integer>>[] futuresArray = new CompletableFuture[threadCount];
 //            开启模拟线程，使用线程池的方式创建CompletableFuture
-        CompletableFuture<Map<String, Integer>> work1 = CompletableFuture.supplyAsync(simuCallable::call);
-        CompletableFuture<Map<String, Integer>> work2 = CompletableFuture.supplyAsync(simuCallable::call);
-        CompletableFuture<Map<String, Integer>> work3 = CompletableFuture.supplyAsync(simuCallable::call);
-        CompletableFuture<Map<String, Integer>> work4 = CompletableFuture.supplyAsync(simuCallable::call);
-        CompletableFuture<Map<String, Integer>> work5 = CompletableFuture.supplyAsync(simuCallable::call);
+        for (int i = 0; i < threadCount; i++) {
+//            创建模拟线程
+            CompletableFuture<Map<String, Integer>> future = CompletableFuture.supplyAsync(simCallable::call);
+//            将线程放入线程数组
+            futuresArray[i] = future;
+        }
 
-        CompletableFuture<Void> result = CompletableFuture.allOf(work1, work2, work3, work4, work5);
+//        CompletableFuture<Map<String, Integer>> work1 = CompletableFuture.supplyAsync(simCallable::call);
+//        CompletableFuture<Map<String, Integer>> work2 = CompletableFuture.supplyAsync(simCallable::call);
+//        CompletableFuture<Map<String, Integer>> work3 = CompletableFuture.supplyAsync(simCallable::call);
+//        CompletableFuture<Map<String, Integer>> work4 = CompletableFuture.supplyAsync(simCallable::call);
+//        CompletableFuture<Map<String, Integer>> work5 = CompletableFuture.supplyAsync(simCallable::call);
+
+//        设置需等待的子线程
+        CompletableFuture<Void> result = CompletableFuture.allOf(futuresArray);
+//        等待线程完成
         result.join();
+//        创建存放子线程返回结果的List
+        List<Map<String, Integer>> resultList = new ArrayList<>();
 //        获取各子线程模拟结果
-        Map<String, Integer> work1Map = work1.get();
-        Map<String, Integer> work2Map = work2.get();
-        Map<String, Integer> work3Map = work3.get();
-        Map<String, Integer> work4Map = work4.get();
-        Map<String, Integer> work5Map = work5.get();
+        for (int j = 0; j < threadCount; j++) {
+            resultList.add(futuresArray[j].get());
+        }
+
+//        Map<String, Integer> work1Map = work1.get();
+//        Map<String, Integer> work2Map = work2.get();
+//        Map<String, Integer> work3Map = work3.get();
+//        Map<String, Integer> work4Map = work4.get();
+//        Map<String, Integer> work5Map = work5.get();
+
 //        存放key
-        List<String> keys = Arrays.asList("s50", "s100", "s150", "s200", "s250", "s300", "s350", "s400", "s450", "s500", "other");
+//        List<String> keys = Arrays.asList("s50", "s100", "s150", "s200", "s250", "s300", "s350", "s400", "s450", "s500", "other");
 //        总模拟次数
         int totalCount = 0;
         for (String key : keys) {
-            int value = work1Map.get(key) + work2Map.get(key) + work3Map.get(key) + work4Map.get(key) + work5Map.get(key);
+            int value = 0;
+            for (Map<String, Integer> integerMap : resultList) {
+                value += integerMap.get(key);
+            }
+
+//            value = work1Map.get(key) + work2Map.get(key) + work3Map.get(key) + work4Map.get(key) + work5Map.get(key);
+
             countMap.put(key, value);
             totalCount += value;
         }
-        ThreadLocal<Integer>[] values = new ThreadLocal[]{s50, s100, s150, s200, s250, s300, s350, s400, s450, s500, other};
+//        ThreadLocal<Integer>[] values = new ThreadLocal[]{s50, s100, s150, s200, s250, s300, s350, s400, s450, s500, other};
         Arrays.asList(values).forEach(ThreadLocal::remove);
 
 //         输出总结果
-        sample.printResult(countMap, simuCount / 100, totalCount);
+        sample.printResult(countMap, simCount / 100, totalCount);
 //         记录结束时间
         long end = DateTimeUtil.getCurMilli();
         System.out.println(end - start);
@@ -114,64 +145,66 @@ public class AnotherSample {
     }
 
     /**
-     * @Description: 输出模拟结果
-     * @Param: [countMap, simuCount, totalCount]
-     * @return: void
-     * @Author: Hongyan Wang
-     * @Date: 2019/9/23 9:58
+     * @return void
+     * @description 输出模拟结果
+     * @params [countMap, simCount, totalCount]
+     * @author Hongyan Wang
+     * @date 2019/9/24 13:56
      */
-    private void printResult(Map<String, Integer> countMap, Integer simuCount, int totalCount) {
+    private void printResult(Map<String, Integer> countMap, Integer simCount, int totalCount) {
 
         System.out.println("输出结果");
 
-        logger4j.info(String.format("50次以内：%s%%;", (double) countMap.get("s50") / simuCount));
-        logger4j.info(String.format("100次以内：%s%%;", (double) countMap.get("s100") / simuCount));
-        logger4j.info(String.format("150次以内：%s%%;", (double) countMap.get("s150") / simuCount));
-        logger4j.info(String.format("200次以内：%s%%;", (double) countMap.get("s200") / simuCount));
-        logger4j.info(String.format("250次以内：%s%%;", (double) countMap.get("s250") / simuCount));
-        logger4j.info(String.format("300次以内：%s%%;", (double) countMap.get("s300") / simuCount));
-        logger4j.info(String.format("350次以内：%s%%;", (double) countMap.get("s350") / simuCount));
-        logger4j.info(String.format("400次以内：%s%%;", (double) countMap.get("s400") / simuCount));
-        logger4j.info(String.format("450次以内：%s%%;", (double) countMap.get("s450") / simuCount));
-        logger4j.info(String.format("500次以内：%s%%;", (double) countMap.get("s500") / simuCount));
-        logger4j.info(String.format("500次以上：%s%%;", (double) countMap.get("other") / simuCount));
+        logger4j.info(String.format("50次以内：%s%%;", (double) countMap.get("s50") / simCount));
+        logger4j.info(String.format("100次以内：%s%%;", (double) countMap.get("s100") / simCount));
+        logger4j.info(String.format("150次以内：%s%%;", (double) countMap.get("s150") / simCount));
+        logger4j.info(String.format("200次以内：%s%%;", (double) countMap.get("s200") / simCount));
+        logger4j.info(String.format("250次以内：%s%%;", (double) countMap.get("s250") / simCount));
+        logger4j.info(String.format("300次以内：%s%%;", (double) countMap.get("s300") / simCount));
+        logger4j.info(String.format("350次以内：%s%%;", (double) countMap.get("s350") / simCount));
+        logger4j.info(String.format("400次以内：%s%%;", (double) countMap.get("s400") / simCount));
+        logger4j.info(String.format("450次以内：%s%%;", (double) countMap.get("s450") / simCount));
+        logger4j.info(String.format("500次以内：%s%%;", (double) countMap.get("s500") / simCount));
+        logger4j.info(String.format("500次以上：%s%%;", (double) countMap.get("other") / simCount));
 
-        switch (simuCount - totalCount / 100) {
+        switch (simCount % (totalCount / 100)) {
             case 0:
                 logger4j.info(String.format("总计模拟:%d次", totalCount));
                 break;
-            default:
+            case 1:
                 logger4j.info("出现数据丢失，请核查原因");
+                break;
+            default:
+                logger4j.info("系统出错，请重试");
         }
     }
 
     /**
-     * @Description: 模拟多线程相关类
-     * @ClassName: SimuCallable
-     * @Author: Hongyan Wang
-     * @Date: 2019/9/23 9:53
+     * @author Hongyan Wang
+     * @description 模拟多线程相关类
+     * @className SimCallable
+     * @date 2019/9/23 9:53
      */
-    class SimuCallable implements Callable<Map<String, Integer>> {
+    class SimCallable implements Callable<Map<String, Integer>> {
         //        卡池集
         private List<List<Integer>> lists;
         //        总模拟次数
-        private Integer simuCount;
+        private Integer simCount;
 
-        public SimuCallable(List<List<Integer>> lists, Integer simuCount) {
+        private SimCallable(List<List<Integer>> lists, Integer simCount) {
             this.lists = lists;
-            this.simuCount = simuCount;
+            this.simCount = simCount;
         }
 
         /**
-         * @Description: 不再使用同步变量，直接将各子线程结果返回，由主线程处理,
+         * @return java.util.Map<java.lang.String, java.lang.Integer>
+         * @description 不再使用同步变量，直接将各子线程结果返回，由主线程处理,
          * 池子是否乱序并不影响结果，若每次模拟都重新生成乱序池子将大幅降低效率，可以一个线程只使用一个乱序池子，但实际意义不大
-         * @Param: []
-         * @return: java.util.Map<java.lang.String, java.lang.Integer>
-         * @Author: Hongyan Wang
-         * @Date: 2019/9/23 9:52
+         * @params []
+         * @author Hongyan Wang
+         * @date 2019/9/23 9:52
          */
         @Override
-        @SuppressWarnings("unchecked")
         public Map<String, Integer> call() {
 //          记录本线程模拟结果集
             Map<String, Integer> countHashMap = new HashMap<>();
@@ -179,7 +212,7 @@ public class AnotherSample {
                 Random random = SecureRandom.getInstanceStrong();
 //              生成乱序池子
                 int[] ranArray = ranArray();
-                for (int j = 0; j < simuCount; j++) {
+                for (int j = 0; j < simCount; j++) {
 //                开始模拟
                     int count = simulateWork(random, lists, ranArray);
                     //TODO 后续需对统计进行优化
@@ -224,15 +257,12 @@ public class AnotherSample {
                 logger4j.info(e.getMessage());
             }
 
-//            将原先的代码改成该模式，提高页面效果
-            String[] keys = new String[]{"s50", "s100", "s150", "s200", "s250", "s300", "s350", "s400", "s450", "s500", "other"};
-            ThreadLocal<Integer>[] values = new ThreadLocal[]{s50, s100, s150, s200, s250, s300, s350, s400, s450, s500, other};
-
             for (int i = 0; i < keys.length; i++) {
 //            将模拟结果放入集合返回
-                countHashMap.put(keys[i], values[i].get());
+                countHashMap.put(keys[i], (Integer) values[i].get());
 //            执行结束，移除所属的ThreadLocal变量，防止出现内存问题
                 values[i].remove();
+//                logger4j.info(Thread.currentThread().getName() + " : " + keys[i] + " : " + values[i].get());
             }
 
 
@@ -241,18 +271,16 @@ public class AnotherSample {
         }
 
         /**
-         * @Description: 生成乱序不重复数组，作为模拟池
-         * @Param: []
-         * @return: int[]
-         * @Author: Hongyan Wang
-         * @Date: 2019/9/23 9:51
+         * @return int[]
+         * @description 生成乱序不重复数组，作为模拟池
+         * @params []
+         * @author Hongyan Wang
+         * @date 2019/9/23 9:51
          */
-        int[] ranArray() {
+        private int[] ranArray() {
             int[] ranArrays = new int[1000];
             try {
-                for (int i = 0; i < 1000; i++) {
-                    ranArrays[i] = i + 1;
-                }
+                for (int i = 0; i < 1000; i++) ranArrays[i] = i + 1;
                 Random r = SecureRandom.getInstanceStrong();
                 for (int i = 0; i < 1000; i++) {
                     int in = r.nextInt(1000 - i) + i;
@@ -267,54 +295,54 @@ public class AnotherSample {
         }
 
         /**
-         * @Description: 核心代码
+         * @return int
+         * @description 核心代码
          * 模拟抽卡，当前为单个池子，根据要求，生成数个不重复的数值集合，
          * 当结果在某个数值集合中时，从目标集合中移除其所在的集合
          * 当前使用连续生成数值的方式
-         * @Param: [random, lists, ranArray]
-         * @return: int
-         * @Author: Hongyan Wang
-         * @Date: 2019/9/23 9:39
+         * @params [random, lists, ranArray]
+         * @author Hongyan Wang
+         * @date 2019/9/23 9:39
          */
         private int simulateWork(Random random, List<List<Integer>> lists, int[] ranArray) {
             //        抽卡数
             int count = 0;
 //                存放单个池子目标集合，内部数个子集合
-            List<List<Integer>> mblist = new ArrayList<>();
-//                存放单个池子目标值的集合，主要为了避免每次模拟都要对mblist进行两次遍历
-            List<Integer> dblist = new ArrayList<>();
+            List<List<Integer>> multiList = new ArrayList<>();
+//                存放单个池子目标值的集合，主要为了避免每次模拟都要对multiList进行两次遍历
+            List<Integer> numList = new ArrayList<>();
 //            对池子进行模拟，抽完一个之后，再抽下一个
             for (List<Integer> list : lists) {
 //                  生成目标数值的开始值
                 int index = 1;
                 for (Integer integer : list) {
 //                      单个子集合
-                    List<Integer> zlist = new ArrayList<>();
+                    List<Integer> singleList = new ArrayList<>();
                     for (int i = 0; i < integer; i++) {
-                        zlist.add(ranArray[index]);
+                        singleList.add(ranArray[index]);
                         index++;
                     }
-                    dblist.addAll(zlist);
-                    mblist.add(zlist);
+                    numList.addAll(singleList);
+                    multiList.add(singleList);
                 }
                 //TODO 模拟部分是花费时间最多的地方，是主要的优化部分
 //            当目标值不为空时进行抽卡
-                while (!dblist.isEmpty()) {
+                while (!numList.isEmpty()) {
 //            开始抽卡
-                    int result = random.nextInt(1000) + 1;
+                    var result = random.nextInt(1000) + 1;
 //                判断是否抽到目标卡
-                    if (dblist.contains(result))
+                    if (numList.contains(result))
 //                    当抽到目标卡时，遍历目标子集合
-                        for (List<Integer> integerList : mblist)
+                        for (List<Integer> integerList : multiList)
 //                        判断目标是否在子集合中
                             if (integerList.contains(result))
 //                            当目标在子集合中时，从目标集合中移除对应子集合内容
-                                dblist.removeAll(integerList);
+                                numList.removeAll(integerList);
 //                抽卡次数+1
                     count++;
                 }
 //            抽完一池，置空子集合
-                mblist.clear();
+                multiList.clear();
             }
             return count;
         }
