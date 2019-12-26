13e0ba61cef6e5caf44efd31d1ff48b872d6beb7
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2019-12-26 11:30:26 +0800

    日志添加方法参数、方法名及执行结果的记录

--- src/main/java/com/springboot/shiro/shiro2spboot/common/aop/LogAopAspect.java
+++ src/main/java/com/springboot/shiro/shiro2spboot/common/aop/LogAopAspect.java
@@ -9,9 +9,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpSession;
 import java.lang.reflect.Method;
@@ -43,13 +45,24 @@ public class LogAopAspect {
         MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
         // 获取方法
         Method method = methodSignature.getMethod();
+        // 获取方法名
+        String methodName = method.getName();
+        // 获取类名
+        String targetName = pjp.getTarget().getClass().getName();
         // 获取方法上面的注解
         LogAnno logAnno = method.getAnnotation(LogAnno.class);
         // 获取操作描述的属性值
         String operateType = logAnno.operateType();
         // 创建一个日志对象(准备记录日志)
         UserLog log = new UserLog();
-        log.setActType(operateType);// 操作说明
+        StringBuilder actType = new StringBuilder();
+        actType.append("类名 : ").append(targetName).append(" : 方法名 : ").append(methodName);
+        if (!StringUtils.isEmpty(operateType))
+            actType.append(" : 方法描述 : ").append(operateType);
+        log.setActType(actType.toString());// 操作说明
+//        设置方法参数
+        String param = getStrParams(pjp, methodSignature);
+        log.setActParams(param);
 
         try {
             // 设置操作人，从session中获取
@@ -65,8 +78,31 @@ public class LogAopAspect {
         log.setIpAddr(ip);
         // 让代理方法执行
         Object result = pjp.proceed();
+//        记录执行结果
+        log.setActResult((String) result);
         userLogService.insertSelective(log);// 添加日志记录
         return result;
+    }
+
+    /**
+     * @description 获取方法的参数
+     * @params [pjp, methodSignature]
+     * @return java.lang.String
+     * @author Hongyan Wang
+     * @date 2019/12/26 10:19
+     */
+    @NotNull
+    private String getStrParams(ProceedingJoinPoint pjp, MethodSignature methodSignature) {
+        String[] paraNames = methodSignature.getParameterNames();
+        Object[] args = pjp.getArgs();
+        StringBuilder sb = new StringBuilder();
+        if (paraNames != null && paraNames.length > 0 && args != null && args.length > 0) {
+            for (int i = 0; i < paraNames.length; i++) {
+                sb.append(paraNames[i]).append(":").append(args[i]).append(",");
+            }
+        }
+        sb.delete(sb.length() - 1, sb.length() + 1);
+        return sb.toString();
     }
 
 }
--- src/main/java/com/springboot/shiro/shiro2spboot/entity/UserLog.java
+++ src/main/java/com/springboot/shiro/shiro2spboot/entity/UserLog.java
@@ -11,13 +11,17 @@ import lombok.ToString;
 @ToString
 public class UserLog implements Serializable {
     private Integer id;
-
+//  操作用户名
     private String username;
-
+//  操作类型
     private String actType;
-
+//  操作时间
     private Date actTime;
-
+//  操作参数
+    private String actParams;
+//  操作结果
+    private String actResult;
+//  客户机ip地址
     private String ipAddr;
 
     private static final long serialVersionUID = 1L;
