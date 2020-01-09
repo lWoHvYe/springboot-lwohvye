08898f195b8d990a24cef0d27c25132168ca0327
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-01-09 22:37:29 +0800

    PageHelper分页插件依旧无效，继续排查问题

--- src/main/java/com/springboot/shiro/shiro2spboot/service/impl/UserLogServiceImpl.java
+++ src/main/java/com/springboot/shiro/shiro2spboot/service/impl/UserLogServiceImpl.java
@@ -52,10 +52,9 @@ public class UserLogServiceImpl implements UserLogService {
     @dataSource(DatabaseType.SLAVE)
     @Override
     public PageInfo<UserLog> list(String username, String startDate, String endDate, int page, int pageSize) {
-        PageInfo<UserLog> pageInfo = PageHelper.startPage(page, pageSize, "act_time desc").doSelectPageInfo(
+        return PageHelper.startPage(page, pageSize, "act_time desc").doSelectPageInfo(
                 () -> userLogMapper.list(username, startDate, endDate)
         );
-        return pageInfo;
     }
 
     @dataSource(DatabaseType.SLAVE)
