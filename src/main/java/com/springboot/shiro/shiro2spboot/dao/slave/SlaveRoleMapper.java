86d03f46d115528b04225618f8bcc5a20f3dc4d7
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-01-11 12:44:28 +0800

    Mybatis多对一，多对多配置

--- src/main/java/com/springboot/shiro/shiro2spboot/dao/slave/SlaveRoleMapper.java
+++ src/main/java/com/springboot/shiro/shiro2spboot/dao/slave/SlaveRoleMapper.java
@@ -1,22 +1,11 @@
 package com.springboot.shiro.shiro2spboot.dao.slave;
 
-import com.springboot.shiro.shiro2spboot.entity.Permission;
 import com.springboot.shiro.shiro2spboot.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 @Mapper
 public interface SlaveRoleMapper {
     Role selectByPrimaryKey(Long id);
 
-    /**
-     * @return java.util.List<com.springboot.shiro.shiro2spboot.entity.Permission>
-     * @description 根据角色Id获取对应的权限列表
-     * @params [id]
-     * @author Hongyan Wang
-     * @date 2019/12/5 10:37
-     */
-    List<Permission> selectPermissionByRoleId(Long id);
 
 }
