19030728416306e0a15f78ee4a31f331690d9bc6
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-01-19 16:52:08 +0800

    添加验证码过期验证

---
+++ src/main/java/com/springboot/shiro/shiro2spboot/common/shiro/CaptchaExpireException.java
@@ -0,0 +1,13 @@
+package com.springboot.shiro.shiro2spboot.common.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+
+/**
+ * @author Hongyan Wang
+ * @packageName com.springboot.shiro.shiro2spboot.common.shiro
+ * @className CaptchaExpireException
+ * @description 自定义验证码过期异常 AuthenticationException为Shiro认证错误的异常,不同错误类型继承该异常即可
+ * @date 2020/1/19 15:40
+ */
+public class CaptchaExpireException extends AuthenticationException {
+}
