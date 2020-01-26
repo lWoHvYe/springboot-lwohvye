0e471e0ee12c896b0bc89bd48fb1d746522728e2
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-01-26 14:36:50 +0800

    手机号格式校验注解，在需校验的字段上加@PhoneNumber

---
+++ src/main/java/com/springboot/shiro/shiro2spboot/common/annotation/PhoneNumber.java
@@ -0,0 +1,29 @@
+package com.springboot.shiro.shiro2spboot.common.annotation;
+
+import com.springboot.shiro.shiro2spboot.common.validator.PhoneNumberValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author Hongyan Wang
+ * @description 定义手机号校验注解
+ * @date 2020/1/26 13:47
+ */
+@Documented
+@Constraint(validatedBy = PhoneNumberValidator.class)
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
+public @interface PhoneNumber {
+    String message() default "Invalid phone number";
+
+    Class[] groups() default {};
+
+    Class[] payload() default {};
+}
