156a543fc10dbeeafb06c0888b34183aaa063a9c
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-01-27 16:22:10 +0800

    解决consumer层日志异常，部分代码调整

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/DubboConsumerApplication.java
@@ -3,9 +3,13 @@ package com.lwohvye.springboot.dubboconsumer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//开启dubbo
 @EnableDubbo
+//开启缓存支持
+@EnableCaching
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
