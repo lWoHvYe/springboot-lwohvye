91931ef2aaafc006c80f7ba07811c60f6f3ade06
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-06 17:56:32 +0800

    布隆过滤器相关配置

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/common/util/RedisUtil.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/common/util/RedisUtil.java
@@ -1,6 +1,9 @@
 package com.lwohvye.springboot.dubboconsumer.common.util;
 
+import com.google.common.base.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
@@ -15,13 +18,11 @@ import java.util.concurrent.TimeUnit;
  * @description Redis 工具类
  * @date 2019/10/6 20:05
  */
+@Component
 public class RedisUtil {
 
-    private RedisTemplate<String, Object> redisTemplate;
-
-    public RedisUtil(RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
     /**
@@ -661,5 +662,31 @@ public class RedisUtil {
             e.printStackTrace();
             return false;
         }
+    }
+
+    /**
+     * 根据给定的布隆过滤器添加值
+     */
+    public <T> void addByBloomFilter(BloomFilterHelper<T> bloomFilterHelper, String key, T value) {
+        Preconditions.checkArgument(bloomFilterHelper != null, "bloomFilterHelper不能为空");
+        int[] offset = bloomFilterHelper.murmurHashOffset(value);
+        for (int i : offset) {
+            redisTemplate.opsForValue().setBit(key, i, true);
+        }
+    }
+
+    /**
+     * 根据给定的布隆过滤器判断值是否存在
+     */
+    public <T> boolean includeByBloomFilter(BloomFilterHelper<T> bloomFilterHelper, String key, T value) {
+        Preconditions.checkArgument(bloomFilterHelper != null, "bloomFilterHelper不能为空");
+        int[] offset = bloomFilterHelper.murmurHashOffset(value);
+        for (int i : offset) {
+            if (!redisTemplate.opsForValue().getBit(key, i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
