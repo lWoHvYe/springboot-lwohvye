c5cd81467697c90fcd2845db0ca3b4497655e18c
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-07 12:05:34 +0800

    添加定时任务，定时更新布隆过滤器中内容

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/DubboConsumerApplication.java
@@ -4,12 +4,15 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //开启dubbo
 @EnableDubbo
 //开启缓存支持
 @EnableCaching
+//开启定时任务
+@EnableScheduling
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
