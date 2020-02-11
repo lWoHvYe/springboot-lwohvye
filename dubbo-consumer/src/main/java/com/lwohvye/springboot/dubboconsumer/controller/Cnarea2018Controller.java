b42d2963044bffd2191f7389cfa3e0ef0d2ab83e
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-11 09:55:48 +0800

    就只调用dao层进行数据查询，对单线程与多线程对比，多线程能够一定程度上提高效率，若再包含业务处理逻辑，提升会更明显

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/controller/Cnarea2018Controller.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/controller/Cnarea2018Controller.java
@@ -40,14 +40,14 @@ public class Cnarea2018Controller {
     private BloomFilterHelper<String> bloomFilterHelper;
 
     /**
-     * @return com.lwohvye.springboot.dubboconsumer.common.util.ResultModel<com.github.pagehelper.PageInfo < com.lwohvye.springboot.dubbo.entity.Cnarea2018>>
+     * @return com.lwohvye.springboot.dubboconsumer.common.util.ResultModel<java.util.List < com.github.pagehelper.PageInfo < com.lwohvye.springboot.dubbointerface.entity.Cnarea2018>>>
      * @description 异步测试方法，province为使用逗号分隔的两位的省级区划名
      * @params [province, page, pageSize]
      * @author Hongyan Wang
      * @date 2020/1/16 22:13
      */
     @LogAnno(operateType = "异步线程测试")
-    @ApiOperation(value = "根据省名获取下属区划", notes = "多个省名使用逗号分隔")
+    @ApiOperation(value = "根据省名获取下属区划,多线程异步", notes = "多个省名使用逗号分隔")
     @ApiImplicitParam(name = "levels", value = "查询区划级别，实际可使用下拉选择 0省、直辖市 1市 2区县 3街道办事处 4社区居委会")
     @PostMapping("/list")
     public ResultModel<List<PageInfo<Cnarea2018>>> list(String province, String levels, int page, int pageSize) {
@@ -68,4 +68,25 @@ public class Cnarea2018Controller {
         );
     }
 
+    /**
+     * @return com.lwohvye.springboot.dubboconsumer.common.util.ResultModel<java.util.List < com.github.pagehelper.PageInfo < com.lwohvye.springboot.dubbointerface.entity.Cnarea2018>>>
+     * @description 单线程模式，用于同多线程对比
+     * @params [province, levels, page, pageSize]
+     * @author Hongyan Wang
+     * @date 2020/2/11 8:50
+     */
+    @LogAnno(operateType = "同步线程测试，与多线程对比")
+    @ApiOperation(value = "根据省名获取下属区划,单线程模式", notes = "多个省名使用逗号分隔")
+    @ApiImplicitParam(name = "levels", value = "查询区划级别，实际可使用下拉选择 0省、直辖市 1市 2区县 3街道办事处 4社区居委会")
+    @PostMapping("/listSingle")
+    public ResultModel<List<PageInfo<Cnarea2018>>> listSingle(String province, String levels, int page, int pageSize) {
+//        设置区划级别，0省、直辖市 1市 2区县 3街道办事处 4社区居委会
+        Integer level = Convert.toInt(levels);
+//      切割字符串
+        List<String> proList = Arrays.stream(province.split(","))
+//                使用布隆过滤器过滤掉不符合条件的省名
+                .filter(pro -> redisUtil.includeByBloomFilter(bloomFilterHelper, "cnareaPro", pro))
+                .collect(Collectors.toList());
+        return new ResultModel<>(cnarea2018Service.listSingle(proList, level, page, pageSize));
+    }
 }
