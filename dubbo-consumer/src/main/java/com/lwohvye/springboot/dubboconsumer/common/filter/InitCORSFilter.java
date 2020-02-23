a8a9d754dbf9c7143210771a543670f95c4f87b7
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-23 13:36:38 +0800

    更新README.md

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/common/filter/InitCORSFilter.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/common/filter/InitCORSFilter.java
@@ -13,28 +13,28 @@ import java.io.IOException;
 
 /**
  * CORSFilter 解决跨域问题
- *
  */
 @Component
 //使用lombok的该注解代替变量的生成
 @Slf4j
 public class InitCORSFilter extends OncePerRequestFilter {
-//  private Logger log = LoggerFactory.getLogger(InitCORSFilter.class);
-  public InitCORSFilter() {
-    log.info("==== 初始化系统允许跨域请求 ====");
-  }
-  /**
-   * 解决跨域：Access-Control-Allow-Origin，值为*表示服务器端允许任意Domain访问请求
-   */
-  @Override
-  protected void doFilterInternal(@NotNull HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException {
+    //  private Logger log = LoggerFactory.getLogger(InitCORSFilter.class);
+    public InitCORSFilter() {
+        log.info("==== 初始化系统允许跨域请求 ====");
+    }
+
+    /**
+     * 解决跨域：Access-Control-Allow-Origin，值为*表示服务器端允许任意Domain访问请求
+     */
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 //    if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-      response.addHeader("Access-Control-Allow-Origin", "*");
-      response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-      response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
-      response.addHeader("Access-Control-Max-Age", "1800");//30 min
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
+        response.addHeader("Access-Control-Max-Age", "1800");//30 min
 //    }
-    filterChain.doFilter(request, response);
-  }
+        filterChain.doFilter(request, response);
+    }
 }
