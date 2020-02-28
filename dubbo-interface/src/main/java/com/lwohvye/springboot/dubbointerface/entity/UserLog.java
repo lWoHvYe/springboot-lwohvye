1b422b009f519155f82daa65f43e6961fb158440
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-28 15:20:42 +0800

    复杂表头设置，待测试

--- dubbo-interface/src/main/java/com/lwohvye/springboot/dubbointerface/entity/UserLog.java
+++ dubbo-interface/src/main/java/com/lwohvye/springboot/dubbointerface/entity/UserLog.java
@@ -13,26 +13,26 @@ import lombok.ToString;
 @Setter
 @ToString
 public class UserLog implements Serializable {
-    @ExcelProperty(value = "记录ID", index = 0)
+    @ExcelProperty(value = {"标识", "记录ID"}, index = 0)
     private Integer id;
     //  操作用户名
-    @ExcelProperty(value = "操作用户名称", index = 1)
+    @ExcelProperty(value = {"操作信息", "操作用户名称"}, index = 1)
     private String username;
     //  操作类型
-    @ExcelProperty(value = "操作类型", index = 2)
+    @ExcelProperty(value = {"操作信息", "操作类型"}, index = 2)
     private String actType;
     //  操作时间
-    @ExcelProperty(value = "操作时间", index = 3)
+    @ExcelProperty(value = {"操作信息", "操作时间"}, index = 3)
     @DateTimeFormat("yyyy年MM月dd日HH时mm分ss秒")
     private Date actTime;
     //  操作参数
-    @ExcelProperty(value = "操作参数", index = 4)
+    @ExcelProperty(value = {"操作信息", "操作参数"}, index = 4)
     private String actParams;
     //  操作结果
-    @ExcelProperty(value = "操作结果", index = 5)
+    @ExcelProperty(value = {"操作信息", "操作结果"}, index = 5)
     private String actResult;
     //  客户机ip地址
-    @ExcelProperty(value = "客户机地址", index = 6)
+    @ExcelProperty(value = {"主机信息", "客户机地址"}, index = 6)
     private String ipAddr;
 
     private static final long serialVersionUID = 1L;
