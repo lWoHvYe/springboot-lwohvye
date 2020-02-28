8699e64b5618f3daba9cf1801083d3bdd6f2191d
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-02-28 14:41:03 +0800

    添加上传下载excel方法，待测试

--- dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/controller/UserLogController.java
+++ dubbo-consumer/src/main/java/com/lwohvye/springboot/dubboconsumer/controller/UserLogController.java
@@ -1,15 +1,23 @@
 package com.lwohvye.springboot.dubboconsumer.controller;
 
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.excel.EasyExcel;
 import com.github.pagehelper.PageInfo;
+import com.lwohvye.springboot.dubboconsumer.common.listener.UserLogExcelListener;
 import com.lwohvye.springboot.dubboconsumer.common.util.ResultModel;
+import com.lwohvye.springboot.dubbointerface.common.util.excel.ExcelUtil;
 import com.lwohvye.springboot.dubbointerface.entity.UserLog;
 import com.lwohvye.springboot.dubbointerface.service.UserLogService;
 import io.swagger.annotations.ApiOperation;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Hongyan Wang
@@ -52,6 +60,32 @@ public class UserLogController {
 //        json.put("result", pageInfo);
 //        return json.toJSONString();
         return new ResultModel<>(userLogService.list(username, startDate, endDate, page, pageSize));
+    }
+
+    /**
+     * @description 文件下载
+     * @params [response]
+     * @return void
+     * @author Hongyan Wang
+     * @date 2020/2/28 13:28
+     */
+    @GetMapping("/download")
+    public void download(HttpServletResponse response) throws IOException {
+//        设置响应信息
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        var fileName = URLEncoder.encode("日志信息" + IdUtil.simpleUUID(), StandardCharsets.UTF_8);
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+        var list = this.list(null, null, 1, 40);
+//        下载文件
+        new ExcelUtil<UserLog>().download(response, list.getData().getList(), new UserLog(), "第一栏");
+    }
+
+    @PostMapping("/upload")
+    public String update(MultipartFile file) throws IOException {
+        EasyExcel.read(file.getInputStream(),UserLog.class,new UserLogExcelListener(userLogService)).sheet().doRead();
+        return "success";
     }
 
 }
